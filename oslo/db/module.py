87ebe9c4cc2a5c9c44210d9fa8bacf56d4b81ba7
devops <devops@MacPro.lan>
2019-11-05 22:06:21 +0800

    增加请求封装和mysql连接封装还未测试

---
+++ oslo/db/module.py
@@ -0,0 +1,54 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import logging 
+from collections import defaultdict 
+LOG = logging.getLogger(__name__)
+
+
+
+class DBPool(object):
+    def __init__(self):
+        self.subdb = defaultdict(list)
+    def add(self, dbname, db):
+        self.subdb[dbname].append(db)
+    def remove(self, dbname, db):
+        self.subdb[dbname].remove(db)
+    def get(self, dbname):
+        return self.subdb[dbname]
+
+db_pool = DBPool()
+
+class mysqlHanlder(object):
+    def __init__(self,dbname=None,dburl=None,dbecho=False,pool_size=10):
+        self.db_name = dbname
+        self.db_url = dburl
+        self.db_echo = dbecho
+        self.db_pool_size = pool_size
+        self._check_db_name()
+    
+    def _check_db_name(self):
+        resp = db_pool.get(self.db_name)
+        if not resp:
+            self._init_engin()
+            self._init_session()     
+            db_pool.add(self.db_name, self.session)
+    
+    def _init_engin(self):
+        self.engin = create_engine(self.db_url,
+                            encoding="utf-8",
+                            echo=self.db_echo,
+                            pool_size=self.db_pool_size,
+                            pool_recycle=3600,
+                            pool_pre_ping=True)
+    def _init_session(self):
+        self.session = scoped_session(sessionmaker(bind=self.engin,expire_on_commit=False))
+    
+    @property
+    def session(self):
+        db_list = db_pool.get(self.db_name)
+        if db_list:
+            db = db_list[0]
+        return db()
+
+    
+    
---
+++ oslo/web/requesthandler.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@Author: Youshumin
+@Date: 2019-10-31 22:58:44
+@LastEditors: Youshumin
+@LastEditTime: 2019-10-31 23:04:32
+@Description:
+'''
+
+import json
+import tornado
+from tornado.escape import json_decode
+from tornado.util import ObjectDict
+
+class MixinRequestHandler(tornado.web.RequestHandler):
+
+    def __init__(self, application, request, **kwargs):
+        self.headers = self.request.headers
+        super(MixinRequestHandler, self).__init__(
+            application, request, **kwargs)
+
+    def write(self, data, jsond=""):
+        if jsond:
+            self.set_header("Context-Type", "text/json;charset=utf-8")
+            self.set_header("Content-Type", "application/json;charset=UTF-8")
+            self.set_header("Cache-Control", "no-cache")
+        super(MixinRequestHandler, self).write(data)
+
+    def send_error(self, msg=None, callback=None, code=500):
+        send_json = dict(statusCode=code, msg=msg or "", data="")
+        send_json_format = json.dumps(send_json, ensure_ascii=True)
+        if callable:
+            send_json_format = "{}({})".format(callback, send_json_format)
+        self.write(send_json_format, True)
+        self.finish()
+
+    def send_ok(self, data=None, callback=None, code=200):
+        send_json = dict(statusCode=code, msg="", data=data)
+        send_json_format = json.dumps(send_json, ensure_ascii=True)
+        if callback:
+            send_json_format = "{}({})".format(callback, send_json_format)
+        self.write(send_json_format)
+        self.finish()
+
+    def get_client_ip(self):
+        xff = self.headers.get("X-Forwarded-For", "")
+        if xff:
+            return xff.split(",")[0]
+        return self.headers.get("X-Real-Ip", "") or self.request.remote_ip
+
+    def request_body(self):
+        data = dict()
+        _req_body = self.request.body
+
+        if _req_body:
+            try:
+                return ObjectDict(json_decode(_req_body))
+            except Exception:
+                pass
+
+        for k in self.request.arguments:
+            v = self.get_argument(k)
+            data[k] = v[0] if len(v) == 1 else v
+        return
+
+    def get_args(self, key, default="", data_type=None):
+        data_type = data_type or unicode
+        try:
+            data = self.get_argument(key)
+            if callable(data_type):
+                return data_type(data)
+            return data
+        except (tornado.web.MissingArgumentError, ValueError):
+            return default
