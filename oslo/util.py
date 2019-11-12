03d615ff55eeb2e0267ac3fa84a04c792d2d058b
root <You>
2019-11-12 16:42:36 +0800

    添加 创建id和数据对象结构化

--- oslo/util.py
+++ oslo/util.py
@@ -4,14 +4,41 @@
 @Author: Youshumin
 @Date: 2019-11-05 12:20:11
 @LastEditors: Youshumin
-@LastEditTime: 2019-11-05 12:24:48
+@LastEditTime: 2019-11-12 16:39:04
 @Description: 
 '''
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
-def to_str(args):
-    if not isinstance(args, basestring):
-        args = str(args)
-    if isinstance(args, unicode):
-        args = args.encode("utf-8")
-    return args
+def to_str(kwstr):
+    kwstr = kwstr
+    if not isinstance(kwstr, basestring):
+        kwstr = str(kwstr)
+    if isinstance(kwstr, unicode):
+        kwstr = kwstr.encode("utf-8")
+
+    return kwstr
+
+
+def create_id():
+    import uuid
+    _id = uuid.uuid4()
+    return "{}".format(_id)
+
+
+def dbObjFormatToJson(self, field_to_expand=[]):
+    fields = {}
+    for field in [
+            x for x in dir(self) if not x.startswith('_') and x != 'metadata'
+    ]:
+        val = self.__getattribute__(field)
+        if not isinstance(val, MethodType):
+            if len(field_to_expand) == 0:
+                fields[field] = val
+                continue
+            if field in field_to_expand:
+                fields[field] = val
+    LOG.debug(fields)
+    return fields
